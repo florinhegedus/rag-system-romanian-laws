bf491e40ada258499beb1399acf1c2b29796ee0b
florinhegedus <florinxhgds@gmail.com>
2025-03-15 22:21:06 +0200

    add Dockerfile for production and development

--- components/component1/scraper/main.py
+++ components/component1/scraper/main.py
@@ -1,1 +1,31 @@
-print("Scrape is working")
+import requests
+from bs4 import BeautifulSoup
+
+
+CODUL_PENAL_LINK = "https://legislatie.just.ro/Public/DetaliiDocument/109855"
+
+
+def main():
+    page = requests.get(CODUL_PENAL_LINK)
+
+    # Save content to file
+    with open('CODUL_PENAL.html', 'wb+') as f:
+        f.write(page.content)
+    
+    with open('CODUL_PENAL.html', 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    # Parse the HTML content
+    soup = BeautifulSoup(content, 'lxml')
+    
+    # Extract all S_ART tags
+    articles = soup.find_all('span', attrs={'class': 'S_ART'})
+
+    # Dictionary to store extracted texts
+    article_texts = {}
+    law_texts = [article.text for article in articles]
+    law_dict = {article.contents[1].text: article.contents[3].text for article in articles}
+
+
+if __name__ == '__main__':
+    main()
